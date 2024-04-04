9e6b989bfd6d268fef4c2a930831d4c0ccaf55a5
akhil <akhil.ce08@gmail.com>
2024-04-04 10:19:53 +0530

    first commit and uploaded files

---
+++ src/Components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../styles.css";
+
+function Header() {
+    return (
+        <header className="header">
+            <h2 className="header--title">Meme Generator</h2>
+            <h4 className="header--project">React Course - Project 3</h4>
+        </header>
+    )
+}
+
+export default Header;
